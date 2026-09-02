c9f6807a880878917a5bd105db943e60cd03cd41
liahrothstein <liahrothstein@gmail.com>
2026-09-02 14:05:36 +0300

    init: add initial modifier cards constant

--- src/shared/consts/initCards.ts
+++ src/shared/consts/initCards.ts
@@ -17,6 +17,35 @@ import rockClimber from '../assets/cards/exercises/rockClimber.png';
 import sitap from '../assets/cards/exercises/sitap.png';
 import squat from '../assets/cards/exercises/squat.jpg';
 import stepping from '../assets/cards/exercises/stepping.png';
+import modifier1 from '../assets/cards/modifiers/1.png';
+import modifier2 from '../assets/cards/modifiers/2.png';
+import modifier3 from '../assets/cards/modifiers/3.png';
+import modifier4 from '../assets/cards/modifiers/4.png';
+import modifier5 from '../assets/cards/modifiers/5.png';
+import modifier6 from '../assets/cards/modifiers/6.png';
+import modifier7 from '../assets/cards/modifiers/7.png';
+import modifier8 from '../assets/cards/modifiers/8.png';
+import modifier9 from '../assets/cards/modifiers/9.png';
+import modifier10 from '../assets/cards/modifiers/10.png';
+import modifier11 from '../assets/cards/modifiers/11.png';
+import modifier12 from '../assets/cards/modifiers/12.png';
+import modifier13 from '../assets/cards/modifiers/13.png';
+import modifier14 from '../assets/cards/modifiers/14.png';
+import modifier15 from '../assets/cards/modifiers/15.png';
+import modifier16 from '../assets/cards/modifiers/16.png';
+import modifier17 from '../assets/cards/modifiers/17.png';
+import modifier18 from '../assets/cards/modifiers/18.png';
+import modifier19 from '../assets/cards/modifiers/19.png';
+import modifier20 from '../assets/cards/modifiers/20.png';
+import modifier21 from '../assets/cards/modifiers/21.png';
+import modifier22 from '../assets/cards/modifiers/22.png';
+import modifier23 from '../assets/cards/modifiers/23.png';
+import modifier24 from '../assets/cards/modifiers/24.png';
+import modifier25 from '../assets/cards/modifiers/25.png';
+import modifier26 from '../assets/cards/modifiers/26.png';
+import modifier27 from '../assets/cards/modifiers/27.png';
+import modifier28 from '../assets/cards/modifiers/28.png';
+import modifier29 from '../assets/cards/modifiers/29.png';
 
 import type { Card } from '../types/card';
 
@@ -40,4 +69,36 @@ export const initExerciseCards: Card[] = [
   { id: 17, image: sitap },
   { id: 18, image: squat },
   { id: 19, image: stepping },
+];
+
+export const initModifierCards: Card[] = [
+  { id: 1, image: modifier1 },
+  { id: 2, image: modifier2 },
+  { id: 3, image: modifier3 },
+  { id: 4, image: modifier4 },
+  { id: 5, image: modifier5 },
+  { id: 6, image: modifier6 },
+  { id: 7, image: modifier7 },
+  { id: 8, image: modifier8 },
+  { id: 9, image: modifier9 },
+  { id: 10, image: modifier10 },
+  { id: 11, image: modifier11 },
+  { id: 12, image: modifier12 },
+  { id: 13, image: modifier13 },
+  { id: 14, image: modifier14 },
+  { id: 15, image: modifier15 },
+  { id: 16, image: modifier16 },
+  { id: 17, image: modifier17 },
+  { id: 18, image: modifier18 },
+  { id: 19, image: modifier19 },
+  { id: 20, image: modifier20 },
+  { id: 21, image: modifier21 },
+  { id: 22, image: modifier22 },
+  { id: 23, image: modifier23 },
+  { id: 24, image: modifier24 },
+  { id: 25, image: modifier25 },
+  { id: 26, image: modifier26 },
+  { id: 27, image: modifier27 },
+  { id: 28, image: modifier28 },
+  { id: 29, image: modifier29 },
 ];
